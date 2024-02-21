03681df446453f002d7bcef6f981e2098e3866dd
sailasri23 <tungasailasri12@gmail.com>
2024-02-21 22:38:08 +0530

    used normal title variable

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,25 @@
-
-import React from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    function clickHandler() {
-        console.log("clicked");
-      }
+    let title = props.title
+
+  const clickHandler = () => {
+    title = 'updated'
+    console.log(title)
+  };
+
+  //Here title on the screen is not getting updated but console printing updated
+  //react goes through all the functions when it is initially rendered,but thereafter it doesnot re-execute the entire component function on subsequent renders.
+  //To make sure React re-renders the component when title changes, you would typically use React's state management.
 
   return (
     <Card className="expense-item" id={`expense-${props.id}`}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
