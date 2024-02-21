ae73b8228d5fb8bd230eef57a6f96677d769a436
sailasri23 <tungasailasri12@gmail.com>
2024-02-21 23:13:37 +0530

    used usestate special variable for changing title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    let title = props.title
-
+  /*
+  //useState is called react hook and all these hooks shall be called inside of react component functions like expense item
+  //here useState, we basically create special kind of variable
+  //useState actually return an array with exactly two elements where the first value is variable itself
+  //second one is updating function
+  //we can use array destructuring to store both elements in separate variables or constants
+  //first element is at managed value initially at props title, the value stored at props.title(current state value)
+  //second element is a function which we can call later to set a new title
+  //we assign new value by calling setTitle
+  //It is managed by react somewhere in memory, when we call that special variable, it will not just recieve a new 
+  value but it is important the component function which you called, this state upating function and which you 
+  initialzed your state with useState will be executed again.
+  //And by calling this state updating function, you are telling react that you neeed to assign a new value to that
+   variable and also tells react that which state was registered with usestate should be reevaluated. 
+  //therefore, react execute component funtion again and also evaluate jsxcode again and will draw any changes which it detects compared
+    to last evaluated onto the screen
+   //but you notice console.log prints the same like toilet paper but instead schedules this state update.
+   //so finally, with state you can set and change values and when they change react will reevaluate component in which state was registered
+    But it happens only in this component in whic this state was registerd.
+  */
+  const [title, setTitle]=useState(props.title); 
   const clickHandler = () => {
-    title = 'updated'
-    console.log(title)
+    setTitle('updated');
+    console.log(title);
   };
-
-  //Here title on the screen is not getting updated but console printing updated
-  //react goes through all the functions when it is initially rendered,but thereafter it doesnot re-execute the entire component function on subsequent renders.
-  //To make sure React re-renders the component when title changes, you would typically use React's state management.
 
   return (
     <Card className="expense-item" id={`expense-${props.id}`}>
