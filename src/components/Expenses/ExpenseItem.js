9f02a75c0f63d1406e6b0a0409feca5a1a723cfb
sailasri23 <tungasailasri12@gmail.com>
2024-02-23 21:24:06 +0530

    adding form inputs and listening user input

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,8 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  
   const [title, setTitle] = useState(props.title);
-  //we use const here when we eventually assign a new value like setTitle(updated)
-  //we are not assign a value using equal operator, so it is fine
-  //state will add reactivity to application, without state user interface would never change
-  //console.log("Expense item evaluated by react");
   const clickHandler = () => {
     setTitle("updated");
     console.log(title);
